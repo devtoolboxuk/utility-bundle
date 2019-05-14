cb2f981913589133b7051ee8f32a6d9db9e4dee9
dev-toolbox <^G6VqtuZK#SD>
2019-05-14 09:24:16 +0100

    Updated execution time script

--- src/Handlers/Debug.php
+++ src/Handlers/Debug.php
@@ -8,7 +8,7 @@ class Debug extends Handler
 
     public function initialise()
     {
-        $this->time_start = microtime(true);
+        $this->time_start = microtime();
     }
 
     /**
@@ -16,9 +16,21 @@ class Debug extends Handler
      */
     public function executionTime()
     {
-        $time_end = microtime(true);
+        $time_end = microtime();
         $execution_time = ($time_end - $this->time_start);
-        return date("H:i:s", $execution_time);
+        return $this->formatPeriod( $execution_time);
     }
 
+    /**
+     * @param $totalSeconds
+     * @return string
+     */
+    private function formatPeriod($totalSeconds) {
+        $startTotalSeconds = $totalSeconds;
+        $hours = floor($totalSeconds / 3600);
+        $totalSeconds %= 3600;
+        $minutes = floor($totalSeconds / 60);
+        $seconds = $startTotalSeconds - ($minutes * 60);
+        return sprintf("%02d",$hours) . ":" . sprintf("%02d",$minutes) . ":" .sprintf("%.3f", $seconds);
+    }
 }
