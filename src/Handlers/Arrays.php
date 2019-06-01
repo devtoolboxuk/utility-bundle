ab9d28923de714780bfcea190da5737d6cee0fcf
Rob Wilson <rob.wilson@supergroup.co.uk>
2019-06-01 10:06:18 +0100

    Array Merging

--- src/Handlers/Arrays.php
+++ src/Handlers/Arrays.php
@@ -6,10 +6,31 @@ class Arrays extends Handler
 {
 
     /**
-     * @param $array
+     * @param array $merged
+     * @param array $array2
      * @return array
      */
-    public function arrayValuesRecursive($array)
+    public function arrayMergeRecursiveDistinct($merged = [], $array2 = [])
+    {
+        if (empty($array2)) {
+            return $merged;
+        }
+
+        foreach ($array2 as $key => &$value) {
+            if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
+                $merged[$key] = $this->arrayMergeRecursiveDistinct($merged[$key], $value);
+            } else {
+                $merged[$key] = $value;
+            }
+        }
+        return $merged;
+    }
+
+    /**
+     * @param array $array
+     * @return array
+     */
+    public function arrayValuesRecursive($array = [])
     {
         $flat = [];
 
