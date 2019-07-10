ffc22de2f37ee5b4389469c111831ad78acf2c0e
robwilson <rob.wilson@supergroup.co.uk>
2019-07-10 15:45:13 +0100

    Fix to exception, and also allows for timestamps

--- src/Handlers/Date.php
+++ src/Handlers/Date.php
@@ -10,6 +10,27 @@ class Date extends Handler
     private $dateFormat = 'Y-m-d H:i:s';
 
     /**
+     * @param integer $date
+     * @return string|null
+     * @throws Exception
+     */
+    private function convertUnixTime($date)
+    {
+        $date = DateTime::createFromFormat('U', $date);
+        return $this->convert($this->getTimeFormat($date));
+    }
+
+    private function isTimestamp($string)
+    {
+        try {
+            new DateTime('@' . $string);
+        } catch(Exception $e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
      * @param $date
      * @return string|null
      * @throws Exception
@@ -20,10 +41,15 @@ class Date extends Handler
             return null;
         }
 
+        if ($this->isTimestamp($date)) {
+            $date = $this->convertUnixTime($date);
+        }
+
         try {
             $dt = new DateTime($date);
         } catch (Exception $x) {
-            throw new Exception("Internally stored date/time/timestamp value could not be converted to DateTime: " . var_export($date, true), $x);
+
+            throw new Exception("Internally stored date/time/timestamp value could not be converted to DateTime: " . var_export($date, true), $x->getCode());
         }
 
         if (strpos($this->dateFormat, '%') !== false) {
@@ -38,7 +64,7 @@ class Date extends Handler
      *
      * @param string $format
      */
-    public function setDateFormat($format = 'Y-m-d H:i:s' )
+    public function setDateFormat($format = 'Y-m-d H:i:s')
     {
         $this->dateFormat = $format;
     }
